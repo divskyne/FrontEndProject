56087e46351891caca88a6171363fa9301868f9c
Divine <divineabraham007@gmail.com>
2018-10-18 02:51:23 +0100

    added basic front end

---
+++ src/Components/counter.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+
+class counter extends Component {
+
+handleSubmit(event) {
+
+   var emailstr =  document.getElementById('uname').value;
+   var passstr = document.getElementById('pword').value;
+
+   const url = "http://localhost:8090/api/login"
+   
+   var user = JSON.stringify({
+       "email":emailstr,
+       "password":passstr
+   })
+
+   console.log(user);
+   
+   let xhttp = new XMLHttpRequest();
+   xhttp.open("POST", url);
+   xhttp.setRequestHeader("Content-type", "application/json");
+   xhttp.setRequestHeader('Access-Control-Allow-Origin','*');
+   xhttp.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
+   xhttp.responseType = 'text';
+   xhttp.send(user);
+
+   xhttp.onload = ()=>{
+    console.log(xhttp.responseText);
+     }
+}
+
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          UserName:
+          <input type="text" name="uname" id = "uname" />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="pword" id="pword"/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default counter;
